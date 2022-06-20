2928fdcd98d369cab40944805f23a981c1bacc60
Pablo Urbano <pablojamiro2008@hotmail.com>
2022-06-20 02:19:21 -0500

    Update: fix en endpoint

--- src/enviroments/env.js
+++ src/enviroments/env.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  // GENERAL_API:   "https://back-esagel.herokuapp.com/api",
-  GENERAL_API: "http://localhost:4000/api",
+  GENERAL_API:   "https://back-esagel.herokuapp.com/api",
+  // GENERAL_API: "http://localhost:4000/api",
 };
