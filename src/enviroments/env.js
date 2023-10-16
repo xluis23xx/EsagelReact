4978b07dbfd0ac86811efb71946b6573477c69ca
xluis23xx <54380953+xluis23xx@users.noreply.github.com>
2023-10-16 16:45:29 -0500

    Update env.js

--- src/enviroments/env.js
+++ src/enviroments/env.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  GENERAL_API:   "https://esagel-back-production.up.railway.app/api",
+  GENERAL_API:   "https://esagel-back-production-b97f.up.railway.app/api",
   // GENERAL_API: "http://localhost:4000/api",
 };
