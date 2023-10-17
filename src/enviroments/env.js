53dc5f5a0bcb6bee01e628c87efd47768ffeec37
Pablo El Maldy <pablojamiro2008@hotmail.com>
2023-10-17 02:40:28 -0500

    update: ajuste en ubigeo

--- src/enviroments/env.js
+++ src/enviroments/env.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  GENERAL_API:   "https://app-esagel-9a003b6f14ce.herokuapp.com//api",
+  GENERAL_API:   "https://app-esagel-9a003b6f14ce.herokuapp.com/api",
   // GENERAL_API: "http://localhost:4000/api",
 };
